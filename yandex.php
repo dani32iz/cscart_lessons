04608ff601346428f9cbb92a20bc5305df472d9d
dbazenov <dbazhenov@hotmail.com>
2015-06-07 12:14:02 +0300

    Скрипт из видео

--- yandex.php
+++ yandex.php
@@ -1,40 +1,34 @@
-#!/usr/bin/php
 <?php
 
-// Подключаем классы ядра
 use Tygh\Registry;
 use Tygh\Ym\Yml;
 
-// Константы
 define('AREA', 'C');
 define('NO_SESSION', true);
-define('YANDEX_CRON_PRINT_RESULT', false);
+define('DEVELOPMENT', true);
 
-// Отображение ошибок
 @ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-// Подключаем ядро
 require dirname(__FILE__) . '/../init.php';
 
-// Получаем ID витрины
 $company_id = Registry::get('runtime.company_id');
 
-// Настройки модуля Яндекс.Маркет
 $options = Registry::get('addons.yandex_market');
 
-$page = !empty($_REQUEST['page']) ? $_REQUEST['page'] : 0;
+$page = 0;
 
 $lang_code = DESCR_SL;
-
 if (Registry::isExist('languages.ru')) {
     $lang_code = 'ru';
 }
 
-$yml = new Yml($company_id, $options, $lang_code, $page, true);
+$yml = new Yml($company_id, $options, $lang_code, $page, false);
 
 $filename = $yml->getFileName();
 
 $yml->generate($filename);
+
+fn_echo('Good');
 
 exit;
